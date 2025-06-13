f079ceb4f86189c11bd226b1e4e5677b0a014951
dmdals1012 <dmdals1012@gmail.com>
2025-06-13 21:00:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def check_password():
         
     if not st.session_state.authenticated:
         st.title("🔒 접근 권한")
-        password = st.number_input("비밀번호를 입력하세요", type="password")
+        password = st.number_input("비밀번호를 입력하세요", format="%d")
         
         if st.button("로그인", use_container_width=True):
             if password == 1234:
